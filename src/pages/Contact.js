37b34b295189cedf04d7d7353ab7559b9d170bc4
Aditya Yadav <adiyadav.902@gmail.com>
2022-12-18 23:26:21 +0530

    4-register

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,22 @@
 import { useForm } from "react-hook-form";
 
 const Contact = () => {
-  const {} = useForm();
+  const { register } = useForm();
+
+  // const {} = register();
 
   return (
     <>
       <h1 className="text-xl">Contact</h1>
       <form>
-        <input type="text" placeholder="First name" />
-        <input type="text" placeholder="Last name" />
-        <input type="text" placeholder="Email" />
-        <input type="tel" placeholder="Mobile number" />
+        <input type="text" placeholder="First name" {...register("fname")} />
+        <input type="text" placeholder="Last name" {...register("lname")} />
+        <input type="text" placeholder="Email" {...register("email")} />
+        <input
+          type="tel"
+          placeholder="Mobile number"
+          {...register("contact")}
+        />
 
         <input type="submit" />
       </form>
