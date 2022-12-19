3a246204f4838dad7bd895a1a5789d36f440d622
Aditya Yadav <adiyadav.902@gmail.com>
2022-12-19 23:00:05 +0530

    5-Handle-Submit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,22 @@
 import { useForm } from "react-hook-form";
 
 const Contact = () => {
-  const { register } = useForm();
+  const { register, handleSubmit } = useForm();
 
   // const {} = register();
+
+  const handleData = () => {
+    console.log("we will hit some random api in this callback");
+  };
+
+  const handleError = () => {
+    //
+  };
 
   return (
     <>
       <h1 className="text-xl">Contact</h1>
-      <form>
+      <form onSubmit={handleSubmit(handleData, handleError)}>
         <input type="text" placeholder="First name" {...register("fname")} />
         <input type="text" placeholder="Last name" {...register("lname")} />
         <input type="text" placeholder="Email" {...register("email")} />
