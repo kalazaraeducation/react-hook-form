b51a63ed49b1bc83a4b33e412fa0e6e2a5516a24
Aditya Yadav <adiyadav.902@gmail.com>
2022-12-21 00:35:18 +0530

    8-Validations

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -29,13 +29,24 @@ const Contact = () => {
     <>
       <h1 className="text-xl">Contact</h1>
       <form onSubmit={handleSubmit(handleData, handleError)}>
-        <input type="text" placeholder="First name" {...register("fname")} />
-        <input type="text" placeholder="Last name" {...register("lname")} />
-        <input type="text" placeholder="Email" {...register("email")} />
         <input
-          type="tel"
+          type="text"
+          placeholder="First name"
+          {...register("fname", { required: true, minLength: 3, maxLength: 8 })}
+        />
+        <input type="text" placeholder="Last name" {...register("lname")} />
+        <input
+          type="text"
+          placeholder="Email"
+          {...register("email", {
+            required: true,
+            pattern: /^([a-zA-Z0-9@*#]{8,15})$/,
+          })}
+        />
+        <input
+          type="number"
           placeholder="Mobile number"
-          {...register("contact")}
+          {...register("contact", { required: true, min: 10, max: 20 })}
         />
 
         <input type="submit" />
